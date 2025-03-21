0fe9fe366d5930bebe293a62312383791854504d
Roman Tyshchenko <rtyshchenko@griddynemics.com>
2025-03-21 17:36:21 +0200

    added 2 d way of circuit breaker into product

--- config-server/src/main/java/com/gd/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/gd/configserver/ConfigServerApplication.java
@@ -3,10 +3,9 @@ package com.gd.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
